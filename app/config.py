b0263c8a463c1c4fe025ba4ddeb884ffb26d53c1
yuriy.kozhev <yuriy.kozhev@gmail.com>
2022-03-27 18:14:30 +0300

    add pika connection params

--- app/config.py
+++ app/config.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import logging
+import pika
 
 APP_ROOT = Path(__file__).parent.resolve()
 
@@ -10,3 +11,5 @@ LOGGING_CONFIG = dict(filename=APP_ROOT.joinpath('app.log'),
 
 PARSING_QUEUE_NAME = 'Parsing'
 ERRORS_QUEUE_NAME = 'Errors'
+
+RABBITMQ_CONN_PARAMS = pika.ConnectionParameters(host='localhost', socket_timeout=5)
