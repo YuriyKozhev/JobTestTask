e06a174b9821c302f643b71e5e13b0a57ada782b
yuriy.kozhev <yuriy.kozhev@gmail.com>
2022-03-27 18:11:47 +0300

    add logging config

--- app/config.py
+++ app/config.py
@@ -1,3 +1,9 @@
 from pathlib import Path
+import logging
 
 APP_ROOT = Path(__file__).parent.resolve()
+
+LOGGING_CONFIG = dict(filename=APP_ROOT.joinpath('app.log'),
+                      level=logging.INFO,
+                      format='%(asctime)s:%(levelname)s:%(message)s',
+                      datefmt='%Y-%m-%d %H:%M:%S')
